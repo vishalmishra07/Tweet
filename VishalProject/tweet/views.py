1dc9954dd803f3a558c123e7453a30e836833e52
Romanov <vishalmishra7828@gmail.com>
2025-02-23 23:30:43 +0530

    tweet push 2

--- VishalProject/tweet/views.py
+++ VishalProject/tweet/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from .models import Tweet
 from .forms import Tweetform
-from django.shortcuts import get_object_or_404
+from django.shortcuts import get_object_or_404,redirect
 # Create your views here.
 def index(request):
     return render(request,'index.html')
@@ -12,7 +12,14 @@ def tweet_list(request):
 def tweet_create(request):
     if request.method == "POST":
         form = Tweetform(request.POST, request.FILES)
+        if form.is_valid():
+            tweet=form.save(commit=False)
+            tweet.user = request.user
+            tweet.save()
+            return redirect('tweet_list')
     else:
         form = Tweetform()
     return render(request,'tweet_form.html',{'form':form})
+
+def tweet_edit(rel)
         
