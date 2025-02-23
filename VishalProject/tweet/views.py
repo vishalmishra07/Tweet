c2dd740b1f5e4777ac0a5a7c46a9ecbd408d7d36
Romanov <vishalmishra7828@gmail.com>
2025-02-23 23:57:48 +0530

    tweet push 3

--- VishalProject/tweet/views.py
+++ VishalProject/tweet/views.py
@@ -21,5 +21,23 @@ def tweet_create(request):
         form = Tweetform()
     return render(request,'tweet_form.html',{'form':form})
 
-def tweet_edit(rel)
+def tweet_edit(request, tweet_id):
+    tweet=get_object_or_404(Tweet,pk=tweet_id,user=request.user)
+    if request.method == 'POST':
+        form = Tweetform(request.POST, request.FILES , instance=tweet)
+        if form.is_valid():
+            tweet = form.save(commit=False)
+            tweet.user=request.user
+            tweet.save()
+            return redirect('tweet_list')
+    else:
+        form = Tweetform(instance=Tweet)
+    return render(request,'tweet_form.html',{'form':form})
+
+def twee_delete(request,tweet_id):
+    get_object_or_404(Tweet,pl=tweet_id, user=request.user)
+    if request.method =='POST':
+        tweet.delete()
+        return redirect('tweet_list')
+    return render(request,'tweet_confirm_delete.html',{'tweet':tweet})
         
