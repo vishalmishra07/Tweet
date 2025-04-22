4bdab4d19f025b22f5cf7319a54a36b8dc1425aa
Romanov <vishalmishra7828@gmail.com>
2025-04-22 18:37:36 +0530

    home page

--- VishalProject/VishalProject/urls.py
+++ VishalProject/VishalProject/urls.py
@@ -24,7 +24,7 @@ from django.contrib.auth.urls import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('tweet/', include('tweet.urls')),
+    path('', include('tweet.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
     
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
